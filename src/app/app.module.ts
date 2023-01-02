0e75e79e118d01a3efa3d34277f5f74e724c6c66
Rafael Reis <rafa.araujoreis@gmail.com>
2023-01-02 09:58:59 -0300

    add new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
+import { ComponentsModule } from './components/components.module';
+import { ViewsModule } from './views/views.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+const modules = [ComponentsModule, ViewsModule];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ...modules],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [...modules],
 })
-export class AppModule { }
+export class AppModule {}
