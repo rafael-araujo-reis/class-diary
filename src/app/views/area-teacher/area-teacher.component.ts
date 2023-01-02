441a8c95dc9e2cffa4574c640ae8088b84708905
Rafael Reis <rafa.araujoreis@gmail.com>
2023-01-02 19:22:40 -0300

    tratativa area do professor

--- src/app/views/area-teacher/area-teacher.component.ts
+++ src/app/views/area-teacher/area-teacher.component.ts
@@ -3,13 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-area-teacher',
   templateUrl: './area-teacher.component.html',
-  styleUrls: ['./area-teacher.component.scss']
+  styleUrls: ['./area-teacher.component.scss'],
 })
 export class AreaTeacherComponent implements OnInit {
+  titlePage = '';
 
-  constructor() { }
+  teacher = {
+    name: 'Danielle Cristina',
+    surname: 'da Silva Reis',
+  };
+
+  listMaterials = [];
+  listStudents = [];
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.definitionTitlePage();
   }
 
+  definitionTitlePage() {
+    const listSizeMaterial = this.listMaterials.length;
+    const listSizeStudents = this.listStudents.length;
+
+    if (listSizeMaterial === 0 && listSizeStudents === 0) {
+      this.titlePage = 'Sua lista de materiais e alunos está vazia';
+    } else if (listSizeMaterial > 0 && listSizeStudents === 0) {
+      this.titlePage = 'Sua lista de alunos está vazia';
+    } else if (listSizeMaterial === 0 && listSizeStudents > 0) {
+      this.titlePage = 'Sua lista de materiais está vazia';
+    }
+  }
 }
