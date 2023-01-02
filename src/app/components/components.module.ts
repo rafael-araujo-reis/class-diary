beeae8b51aea32cd0857dadfe7e74fe9ad6ba99a
Rafael Reis <rafa.araujoreis@gmail.com>
2023-01-02 09:58:41 -0300

    add structure component menu

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuTabsComponent } from './menu-tabs/menu-tabs.component';
+
+@NgModule({
+  declarations: [MenuTabsComponent],
+  imports: [CommonModule],
+})
+export class ComponentsModule {}
