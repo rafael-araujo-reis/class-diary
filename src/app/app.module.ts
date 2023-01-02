8f9a8a861819ef6d9c40110583132f0fd8115fa6
Rafael Reis <rafa.araujoreis@gmail.com>
2023-01-02 12:11:08 -0300

    consumer menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
+import { MenuTabsComponent } from './components/menu-tabs/menu-tabs.component';
 import { ComponentsModule } from './components/components.module';
 import { ViewsModule } from './views/views.module';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 const modules = [ComponentsModule, ViewsModule];
+const material = [
+  BrowserAnimationsModule,
+  MatIconModule,
+  BrowserModule,
+  FormsModule,
+  HttpClientModule,
+];
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ...modules],
+  declarations: [AppComponent, MenuTabsComponent],
+  imports: [BrowserModule, AppRoutingModule, ...modules, ...material],
   providers: [],
   bootstrap: [AppComponent],
   exports: [...modules],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
