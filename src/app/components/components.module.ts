71a7678e75ec271b05c2ae26e62fb4fcdcbc70da
Rafael Reis <rafa.araujoreis@gmail.com>
2023-01-05 22:40:31 -0300

    add new component loader

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { ButtonComponent } from './button/button.component';
+import { LoaderComponent } from './loader/loader.component';
 
-const components = [ButtonComponent];
+const components = [ButtonComponent, LoaderComponent];
 @NgModule({
-  declarations: [...components],
+  declarations: [...components, ],
   imports: [CommonModule, MatIconModule],
   exports: [...components],
 })
