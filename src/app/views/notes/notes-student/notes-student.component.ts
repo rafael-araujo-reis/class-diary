7dbce02811bc3e4fe4ad85a5d7fba5534d4e7434
Rafael Reis <rafa.araujoreis@gmail.com>
2023-01-12 08:40:14 -0300

    create layout list notes student

--- src/app/views/notes/notes-student/notes-student.component.ts
+++ src/app/views/notes/notes-student/notes-student.component.ts
@@ -1,15 +1,52 @@
+import { IListStudents } from './../../../interfaces/IStudents.interface';
+import { StudentsService } from './../../../services/students.service';
+import { ActivatedRoute } from '@angular/router';
+import { RouterNavigate } from './../../../shared/utils/router-navigate';
+import { RouterEnum } from './../../../shared/constants/RouterEnum.enum';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notes-student',
   templateUrl: './notes-student.component.html',
-  styleUrls: ['./notes-student.component.scss']
+  styleUrls: ['./notes-student.component.scss'],
 })
 export class NotesStudentComponent implements OnInit {
+  student: IListStudents | undefined;
+  id: string = '';
+  loading: boolean = true;
 
-  constructor() { }
+  constructor(
+    private router: RouterNavigate,
+    private routeActive: ActivatedRoute,
+    private studentsService: StudentsService
+  ) {}
 
   ngOnInit(): void {
+    this.routeActive.queryParams.subscribe((params) => {
+      this.id = params['id'] || '';
+    });
+
+    this.getDetailsStudentById(this.id);
   }
 
+  goToHomeDiary() {
+    this.router.navigatePath(RouterEnum.HOME_DIARY);
+  }
+
+  getDetailsStudentById(id: string) {
+    return this.studentsService
+      .getDetailsStudentById(id)
+      .subscribe((student: IListStudents) =>
+        this.updateDetailsStudent(student)
+      );
+  }
+  updateDetailsStudent(student: IListStudents) {
+    this.student = student;
+    this.loading = false;
+    console.log(this.student);
+  }
+
+  // get searchNotesStudents(id: string){
+  //   return this.notesServices
+  // }
 }
