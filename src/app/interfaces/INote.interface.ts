ea1ce1acaafafdaf341e5f6b07351cb1b7f2d844
Rafael Reis <rafa.araujoreis@gmail.com>
2023-01-12 09:15:47 -0300

    create interface notes

---
+++ src/app/interfaces/INote.interface.ts
@@ -0,0 +1,9 @@
+export interface INote {
+  idStudent: string;
+  idNote: string;
+  idSchoolSubject: string;
+  activityDate: Date;
+  activitySubject: string;
+  pageNumber: string;
+  additionalInformation: string;
+}
